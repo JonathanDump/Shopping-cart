ce16c7501fac41a2e0750160a2ee35e6d4ffb0d3
Jonathan Dump <jbrodii77@gmail.com>
2023-07-26 12:59:09 +0300

    Change json file location

--- src/hooks/useProductLoader.js
+++ src/hooks/useProductLoader.js
@@ -2,14 +2,13 @@ import { useState, useEffect, useReducer } from "react";
 import { reducer } from "../functions/reducer";
 
 export function useProductLoader() {
-  //   const [products, setProducts] = useState(null);
   const [loading, setLoading] = useState(true);
   const [products, dispatch] = useReducer(reducer, null);
 
   useEffect(() => {
     async function productLoader() {
       try {
-        const response = await fetch("/src/json/products.json");
+        const response = await fetch("/public/json/products.json");
 
         if (!response.ok) {
           throw new Error("Error");
